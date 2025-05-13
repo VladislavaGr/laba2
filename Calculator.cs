4f7d689af2e0b59f867ffa9de10b6e0cb90055c1
BobinVladislavv <vlad.bobin17@gmail.com>
2025-05-13 10:21:29 +0300

    Добавление функций суммирования и умножения для калькулятора

---
+++ Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Calculator
+﻿using System;
+
+namespace Calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Calculator calc = new Calculator();
+            double result = calc.Add(5, 3);
+            Console.WriteLine("Add Result: " + result);
+            result = calc.Mul(5, 3);
+            Console.WriteLine("Mul Result: " + result);
         }
     }
 }
